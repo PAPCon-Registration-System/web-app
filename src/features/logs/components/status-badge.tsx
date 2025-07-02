03d80c39fdaf8606fc51ad54128368086edfcac2
Samuel-Bonghanoy <20103261@usc.edu.ph>
2025-07-02 16:48:28 +0800

    style: fix text color on logs badges

--- src/features/logs/components/status-badge.tsx
+++ src/features/logs/components/status-badge.tsx
@@ -9,20 +9,20 @@ export default function StatusBadge({ status }: StatusBadgeProps) {
 	switch (status) {
 		case "checked-in":
 			return (
-				<Badge className="bg-success text-success-foreground hover:bg-success/90">
-					checked-in
+				<Badge className="bg-success text-black hover:bg-success/90">
+					Checked-in
 				</Badge>
 			);
 		case "registered":
 			return (
-				<Badge className="bg-analytics-warning text-white hover:bg-analytics-warning-dark">
-					registered
+				<Badge className="bg-analytics-warning text-black hover:bg-analytics-warning-dark">
+					Registered
 				</Badge>
 			);
 		case "no-show":
 			return (
-				<Badge className="bg-analytics-danger text-white hover:bg-analytics-danger-dark">
-					no-show
+				<Badge className="bg-analytics-danger text-black hover:bg-analytics-danger-dark">
+					No-show
 				</Badge>
 			);
 		default:
