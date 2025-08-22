c9dc2a30c97975d16677ea2a96fd748515268e00
0-BSCode <bryanipsanchez@gmail.com>
2025-08-22 20:13:47 +0800

    refactor(frontend): remove unused navbar items and revert reg form width limit

--- src/features/shared/layout/header.tsx
+++ src/features/shared/layout/header.tsx
@@ -1,15 +1,8 @@
 "use client";
-import { Bell, LogOut } from "lucide-react";
+import { LogOut } from "lucide-react";
 import { Button } from "../components/base/button";
 import { ModeToggle } from "../components/base/theme-toggle";
 import { SidebarTrigger } from "../components/base/sidebar";
-import { Separator } from "../components/base/separator";
-import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbList,
-	BreadcrumbPage,
-} from "../components/base/breadcrumb";
 import { authClient } from "@/infrastructure/auth/auth-client";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
@@ -38,23 +31,10 @@ export function Header() {
 						<div className="mr-2">
 							<SidebarTrigger />
 						</div>
-						<Separator orientation="vertical" className="mr-2 h-4" />
-						<Breadcrumb>
-							<BreadcrumbList>
-								<BreadcrumbItem>
-									<BreadcrumbPage className="font-semibold">
-										Dashboard
-									</BreadcrumbPage>
-								</BreadcrumbItem>
-							</BreadcrumbList>
-						</Breadcrumb>
 					</div>
 
 					<div className="flex items-center space-x-2">
 						<ModeToggle />
-						<Button variant="ghost" size="icon">
-							<Bell className="h-5 w-5" />
-						</Button>
 						<Button variant="ghost" size="icon" onClick={handleSignout}>
 							<LogOut />
 						</Button>
